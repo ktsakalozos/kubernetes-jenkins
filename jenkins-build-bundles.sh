dc9ca916fae42efab4dcb1da27e2447376b7c927
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2017-03-07 11:56:08 +0200

    remove bundle dir so git clone will go through

--- jenkins-build-bundles.sh
+++ jenkins-build-bundles.sh
@@ -26,6 +26,7 @@ export JUJU_REPOSITORY=${SCRIPT_DIR}/charms
 source ${SCRIPT_DIR}/define-juju.sh
 
 BUNDLE_REPOSITORY="https://github.com/ktsakalozos/bundle-canonical-kubernetes.git"
+rm -rf bundle
 git clone ${BUNDLE_REPOSITORY} bundle
 
 bundle/bundle -o ./bundles/cdk-flannel -c ${CHANNEL} k8s/cdk cni/flannel
